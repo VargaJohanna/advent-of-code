ef66b6173b296d56a566819277d7cccf3ea67c05
johanna.varga <varga.johanna@gmail.com>
2017-12-07 22:42:06 +0000

    Day 2 second half solution

--- src/main/java/day2/CorruptionChecksum.java
+++ src/main/java/day2/CorruptionChecksum.java
@@ -25,4 +25,32 @@ public class CorruptionChecksum {
         }
         return sumOfDiffs;
     }
+
+    public int findEvenlyDivisibleValues(String input) {
+        int division = 0;
+        int sumOfDivisions = 0;
+
+        for(String line : input.split("\\n")){
+            ArrayList<Integer> listOfNums = new ArrayList<Integer>();
+            for(String num : line.split("\\t")){
+                Integer number = Integer.valueOf(num);
+                listOfNums.add(number);
+            }
+            for(int i = 0; i < listOfNums.size(); i++) {
+                for(int j = i + 1; j < listOfNums.size(); j++) {
+                    if(listOfNums.get(i) / listOfNums.get(j) < 1) {
+                        if(listOfNums.get(j) % listOfNums.get(i) == 0) {
+                            division = listOfNums.get(j) / listOfNums.get(i);
+                        }
+                    } else {
+                        if(listOfNums.get(i) % listOfNums.get(j) == 0) {
+                            division = listOfNums.get(i) / listOfNums.get(j);
+                        }
+                    }
+                }
+            }
+            sumOfDivisions += division;
+        }
+        return sumOfDivisions;
+    }
 }
